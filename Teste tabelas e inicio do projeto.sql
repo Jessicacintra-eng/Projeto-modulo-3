ec1d312900e9debacd4e56106742f5c2f5bd1034
Jessica Cintra <jessica.cintra0712@gmail.com>
2022-05-27 14:17:17 -0300

    inicio do repositório

---
+++ Teste tabelas e inicio do projeto.sql
@@ -0,0 +1,7 @@
+USE game_of_thrones;
+SELECT * FROM game_of_thrones.`characters_v4`;  
+SELECT * FROM game_of_thrones.`got_episodes_v4`;  
+SELECT * FROM game_of_thrones.`houses_v1`; 
+SELECT * FROM game_of_thrones.`killer_records`;
+
+
