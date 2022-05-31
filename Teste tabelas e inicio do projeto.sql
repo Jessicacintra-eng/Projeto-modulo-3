a8cb810a0a9cf18a999d31389ac36bea92943320
BrunaRavelli-byte <brunaravelli13@gmail.com>
2022-05-31 15:17:48 -0300

    perguntas

--- Teste tabelas e inicio do projeto.sql
+++ Teste tabelas e inicio do projeto.sql
@@ -4,4 +4,28 @@ SELECT * FROM game_of_thrones.`got_episodes_v4`;
 SELECT * FROM game_of_thrones.`houses_v1`; 
 SELECT * FROM game_of_thrones.`killer_records`;
 
+SELECT 
+	`got_screentimes`.`name`, 
+    `got_screentimes`.`episodes`,
+    `got_screentimes`.`screentime`
+FROM 
+	`got_screentimes`
+WHERE 
+	`got_screentimes`.`name` LIKE ("%Daenerys%") OR
+    `got_screentimes`.`name` LIKE ("%Cersei%") OR
+    `got_screentimes`.`name` LIKE ("%Arya%")
+ORDER BY 
+	`got_screentimes`.`screentime` DESC;
+    
+    ## considerando o número total de episódios e seu tempo total, qual a porcentagem de aparição de cada personagem em tempo de tela?
+SELECT 
+	`got_screentimes`.`name`, 
+    `got_screentimes`.`episodes`,
+    `got_screentimes`.`screentime`,
+	 ROUND(((`got_screentimes`.`screentime`/(SELECT SUM(screentime) FROM got_screentimes))*100),3)  Porcentagem_Total_tempos_de_tela_da_série
+FROM 
+	`got_screentimes`
+ORDER BY 
+	`got_screentimes`.`name` DESC;
+
 
