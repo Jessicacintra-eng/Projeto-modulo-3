9297774a87b97fa2aec5e2ed6dbbb7bde7891a8d
Ana Luiza <100290512+Ana-Luiza-Ferreira@users.noreply.github.com>
2022-06-03 15:58:27 -0300

    Update pergunta.sql

--- pergunta.sql
+++ pergunta.sql
@@ -18,14 +18,3 @@ ORDER BY
 	`got_screentimes`.`episodes` DESC;
 
 SELECT * FROM game_of_thrones.houses_v1;
-#Número de casas por região
-SELECT
-region, 
-count(house_name)
-FROM houses_v1
-Group by 
-region 
-HAVING
-count(House_name) >= 1 
-order by
-count(house_name) DESC
